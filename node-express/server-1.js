b515d148cf0a93ea110b6def46900ee4b1b8a629
gonzaga8200 <gonzaga8200@gmail.com>
2016-03-31 23:06:36 +0200

    First Commit

---
+++ node-express/server-1.js
@@ -0,0 +1,23 @@
+/**
+ * Created by gonzalomorenominguito on 29/3/16.
+ */
+var express = require('express'),
+    http = require('http');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(function (req, res, next) {
+    console.log(req.headers);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<html><body><h1>Hello World</h1></body></html>');
+
+});
+
+var server = http.createServer(app);
+
+server.listen(port, hostname, function(){
+    console.log('Server running with Express at http://${hostname}:${port}/');
+});
---
+++ rectangle-1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by gonzalomorenominguito on 20/3/16.
+ */
+
+exports.perimeter = function(x,y){
+    return (2*(x+y));
+}
+exports.area = function(x,y){
+    return (x*y);
+}
